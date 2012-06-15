3f4baccea2011f6af58f86e5c69aad9e7b879502
Liam O'Connor <liamoc@cse.unsw.edu.au>
2012-06-16 07:58:43 +1000

    Added BasicIO backend
    BasicIO backend does not support passwords or default text, but it is very simply defined in terms of standard library functions, so is likely to be the most widely compatible.

---
+++ System/Console/Wizard/BasicIO.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DeriveDataTypeable, FlexibleInstances, GADTs, KindSignatures #-}
+module System.Console.Wizard.BasicIO
+        ( BasicIO
+        , runBasicIO
+        ) where
+import System.Console.Wizard
+import System.Console.Wizard.Internal 
+import Control.Monad.Trans
+import Control.Monad.Prompt
+import Control.Monad.Trans.Maybe
+import Control.Applicative((<$>))
+
+import Data.Typeable
+
+-- | A very simple standard IO backend for @wizards@, supporting input and output.
+--   Default text and password masks are ignored.
+--   A more full-featured back-end is based on Haskeline.
+--   Arbitrary IO actions can be performed in wizards via a 'MonadIO' instance.
+data BasicIO (m :: * -> *) r = ArbitraryIO (IO r)
+
+-- | Runs a Wizard action in the BasicIO backend.
+runBasicIO :: Wizard BasicIO a -> IO (Maybe a)
+runBasicIO (Wizard (MaybeT c)) = runRecPromptM f c
+  where f :: WizardAction BasicIO (RecPrompt (WizardAction BasicIO) ) c -> IO c
+        f (Line s) = getLine
+        f (Character s) = getChar
+        f (Password s m) = getLine
+        f (LinePreset s f b) = getLine
+        f (Output s) = putStr s
+        f (OutputLn s) = putStrLn s
+        f (Backend (ArbitraryIO a)) = a
+
+instance MonadIO (Wizard BasicIO) where
+    liftIO = prompt . Backend . ArbitraryIO
